9972d2a817ae6822fa88a7114ed9c54f4bc830af
SwetaSays <swetajhaxx@gmail.com>
2025-09-01 20:37:19 +0530

    movie app

--- lib/widgets/movie_card.dart
+++ lib/widgets/movie_card.dart
@@ -7,11 +7,12 @@ class MovieCard extends StatelessWidget {
   final VoidCallback onTap;
   final VoidCallback onBookmark;
 
-  const MovieCard(
-      {super.key,
-      required this.movie,
-      required this.onTap,
-      required this.onBookmark});
+  const MovieCard({
+    super.key,
+    required this.movie,
+    required this.onTap,
+    required this.onBookmark,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -19,30 +20,72 @@ class MovieCard extends StatelessWidget {
       onTap: onTap,
       child: Container(
         width: 140,
-        margin: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            )
+          ],
+        ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            movie.posterPath != null
-                ? Image.network(
-                    '${IMAGE_BASE_URL}${movie.posterPath}',
-                    height: 180,
-                    fit: BoxFit.cover,
-                  )
-                : Container(height: 180, color: Colors.grey),
-            const SizedBox(height: 5),
-            Text(
-              movie.title,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-            IconButton(
-                icon: Icon(
-                  movie.isBookmarked == 1
-                      ? Icons.bookmark
-                      : Icons.bookmark_border,
-                  color: Colors.red,
+            // Poster with bookmark button
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                  child: movie.posterPath != null
+                      ? Image.network(
+                          '$IMAGE_BASE_URL${movie.posterPath}',
+                          height: 180,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        )
+                      : Container(
+                          height: 180,
+                          width: double.infinity,
+                          color: Colors.grey,
+                          child: const Icon(Icons.image, size: 50, color: Colors.white),
+                        ),
                 ),
-                onPressed: onBookmark)
+
+                // Bookmark floating
+                Positioned(
+                  top: 6,
+                  right: 6,
+                  child: GestureDetector(
+                    onTap: onBookmark,
+                    child: Icon(
+                      movie.isBookmarked == 1
+                          ? Icons.bookmark
+                          : Icons.bookmark_border,
+                      color: Colors.deepOrange,
+                      size: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            // Movie title below
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                movie.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
           ],
         ),
       ),
